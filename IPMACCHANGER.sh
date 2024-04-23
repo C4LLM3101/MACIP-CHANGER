e346907e863c661595a8da2a968bf29fad82d5e0
raistar <sitapradivyan01@gmail.com>
2024-04-23 10:01:54 +0530

    front updated

--- IPMACCHANGER.sh
+++ IPMACCHANGER.sh
@@ -4,8 +4,8 @@
 echo -e "\033[33m      ▄█     ▄███████▄   ▄▄▄▄███▄▄▄▄      ▄████████  ▄████████ "
 echo -e "\033[33m     ███    ███    ███ ▄██▀▀▀███▀▀▀██▄   ███    ███ ███    ███ "
 echo -e "\033[33m     ███▌   ███    ███ ███   ███   ███   ███    ███ ███    █▀  "
-echo -e "\033[33m     ███▌   ███    ███ ███   ███   ███   ███    ███ ███        "
-echo -e "\033[33m     ███▌ ▀█████████▀  ███   ███   ███ ▀███████████ ███        "
+echo -e "\033[35m     ███▌   ███    ███ ███   ███   ███   ███    ███ ███        "
+echo -e "\033[35m     ███▌ ▀█████████▀  ███   ███   ███ ▀███████████ ███        "
 echo -e "\033[33m     ███    ███        ███   ███   ███   ███    ███ ███    █▄  "
 echo -e "\033[33m     ███    ███        ███   ███   ███   ███    ███ ███    ███ "
 echo -e "\033[33m     █▀    ▄████▀       ▀█   ███   █▀    ███    █▀  ████████▀  "
@@ -18,15 +18,15 @@ read mac_choice
 
 # Check if the input is empty
 if [[ -z "$mac_choice" ]]; then
-    echo -e "\033[33m[?] No input provided. Exiting this program. .......>>\033[0m"
+    echo -e "\033[33m[∞] No input provided. Exiting this program. .......>>\033[0m"
     exit 1
 fi
 
 
 if [[ "$mac_choice" == "yes" ]]; then
     
-    echo -e "\033[33m[?] First copy your MAC address and then paste it here as input \033[0m"
-    echo -e "\033[33m[?] Enter the your new MAC address:\033[0m"
+    echo -e "\033[33m[∞] First copy your MAC address and then paste it here as input \033[0m"
+    echo -e "\033[33m[∞] Enter the your new MAC address:\033[0m"
     read new_mac
 
     # Change the MAC address
@@ -36,11 +36,11 @@ if [[ "$mac_choice" == "yes" ]]; then
     ifconfig eth0 up
     ifconfig
 
-    echo -e "\033[33m[?] MAC address changed successufully.......>>\033[0m"
+    echo -e "\033[33m[∞] MAC address changed successufully.......>>\033[0m"
 else 
     
-    echo -e "\033[33m[?] First copy your Private IP address and then paste it here as input \033[0m"
-    echo -e "\033[33m[?] Enter the new Private IP address:\033[0m"
+    echo -e "\033[33m[∞] First copy your Private IP address and then paste it here as input \033[0m"
+    echo -e "\033[33m[∞] Enter the new Private IP address:\033[0m"
     read new_ip
 
     # Change the IP address
@@ -50,5 +50,5 @@ else
     echo -e "\033[33m[?] Private IP address changed successufully.......>>\033[0m"
     
 fi
- echo -e "\033[33m[?] After running this program restart your system so that you can see your output \033[0m"
-  echo -e "\033[33m[?] done...................>> \033[0m"
+ echo -e "\033[33m[∞] After running this program restart your system so that you can see your output \033[0m"
+  echo -e "\033[33m[∞] done...................>> \033[0m"
